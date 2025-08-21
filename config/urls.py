b2f0523d038018824ad9e209d5266e8c0111cf58
Eldor Karimov <hacktivist19021@gmail.com>
2025-08-21 03:09:01 -0400

    changed base urls

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.views.static import serve
 from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
 
 urlpatterns = i18n_patterns(
     re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
@@ -17,3 +18,7 @@ urlpatterns = i18n_patterns(
 
     prefix_default_language=False
 )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
