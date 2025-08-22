1e08718dccb2e93a4516cd55ab7ac91c4db4a9a7
Eldor Karimov <hacktivist19021@gmail.com>
2025-08-22 15:47:07 +0500

    changed settings static files

--- config/settings.py
+++ config/settings.py
@@ -121,12 +121,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [
-        BASE_DIR / 'static'
-    ]
-else:
-    STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
